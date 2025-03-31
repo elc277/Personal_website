350bb4307e4eb6bc8bc29f9822c16da6fcb43f5c
Eftimie Luca Cristian <l.eftimie@student.vu.nl>
2025-03-31 17:39:32 +0200

    changed the background colour and added linkedIn to the contact page.-Luca

--- backend/data/projects.py
+++ backend/data/projects.py
@@ -5,11 +5,6 @@ projects = [
         "link": "https://github.com/VU-Applied-Programming-for-AI-2024/Group-6-"
     },
     {
-        "title": "Q-Learning Maze Agent",
-        "description": "A C# Q-learning agent that navigates a maze.",
-        "link": "https://github.com/yourusername/q-learning-maze"
-    },
-    {
         "title": "Transformer Model - Shakespeare Generator",
         "description": "Developed a lightweight character-level Transformer model from scratch in PyTorch to generate Shakespearean-style text. Trained on a custom dataset, with evaluations on perplexity, accuracy, sentiment, and style reproduction. Focused on balancing performance and efficiency under limited resources.",
         "link": "https://github.com/elc277/LLM"
--- frontend/src/pages/ContactPage.js
+++ frontend/src/pages/ContactPage.js
@@ -1,23 +1,43 @@
 import React from 'react';
 import './ContactPage.css';
 import BackButton from '../components/BackButton';
+import { FaLinkedin, FaEnvelope} from 'react-icons/fa';
 
 const ContactPage = () => {
   return (
     <div className="contact-page">
       <h1 className="section-title">Contact</h1>
+
       <p className="contact-text">
         Want to work together or just say hi?
       </p>
-      <a
-        href="mailto:lucas.eftimie@gmail.com"
-        className="contact-link"
-      >
-        lucas.eftimie@gmail.com
-      </a>
+
+      <div className="contact-block">
+        <FaEnvelope className="contact-icon" />
+        <a
+          href="mailto:lucas.eftimie@gmail.com"
+          className="contact-link"
+        >
+          lucas.eftimie@gmail.com
+        </a>
+      </div>
+
+      <div className="linkedin-block">
+        <FaLinkedin className="linkedin-icon" />
+        <a
+          href="https://www.linkedin.com/in/luca-cristian-eftimie/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="linkedin-link"
+        >
+          linkedin.com/in/luca-cristian-eftimie
+        </a>
+      </div>
+
       <p className="location">
         <span role="img" aria-label="location">📍</span> Amsterdam, Netherlands
       </p>
+
       <BackButton />
     </div>
     
