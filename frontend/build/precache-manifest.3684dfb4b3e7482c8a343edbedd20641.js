5ec12bfa0b3553c30850153aef9d38a7e4ab84db
Eftimie Luca Cristian <l.eftimie@student.vu.nl>
2025-04-01 13:13:07 +0200

    created build.-Luca

---
+++ frontend/build/precache-manifest.3684dfb4b3e7482c8a343edbedd20641.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1d93093408d59da861bfbf0e5981ed4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f55c03d9bc5dc1290712",
+    "url": "/static/css/main.8266effb.chunk.css"
+  },
+  {
+    "revision": "f7c6e0a4a5ea4ad059c6",
+    "url": "/static/js/2.437e6c35.chunk.js"
+  },
+  {
+    "revision": "f55c03d9bc5dc1290712",
+    "url": "/static/js/main.5045ea9f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
